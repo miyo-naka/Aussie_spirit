a93d203099a61a497065f9a5c3a5e6264427abfa
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2025-02-13 15:55:23 +1000

    created pages

--- src/components/Header/page.tsx
+++ src/components/Header/page.tsx
@@ -4,21 +4,25 @@ export function Header() {
   return (
     <header className="flex items-center justify-between h-12 leading-12 bg-amber-900 sticky top-0">
       <div className="ml-2.5 text-3xl text-white no-underline">
-        <p className="font-hurricane">Aussie Spirit</p>
+        <p>
+          <Link href="/" className="font-hurricane">
+            Aussie Spirit
+          </Link>
+        </p>
       </div>
       <nav>
         <ul className="flex items-center space-x-6 mr-6 text-white">
           <li>
-            <Link href="/australia">Aussie Life</Link>
+            <Link href="/AussieLife">Aussie Life</Link>
           </li>
           <li>
-            <Link href="/english">English</Link>
+            <Link href="/English">English</Link>
           </li>
           <li>
-            <Link href="/parenting">Parenting</Link>
+            <Link href="/Parenting">Parenting</Link>
           </li>
           <li>
-            <Link href="/about">About us</Link>
+            <Link href="/About">About us</Link>
           </li>
         </ul>
       </nav>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,10 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-    },
-    fontFamily: {
-      hurricane: "Hurricane",
-      sanserif: "sans-serif",
+      fontFamily: {
+        hurricane: ["Hurricane", "serif"],
+      },
     },
   },
   plugins: [],
