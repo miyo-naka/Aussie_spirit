77b35a72a2166329ea4c8f24b1c11c749586b930
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2025-02-17 11:26:59 +1000

    create Headline component

--- src/app/AussieLife/page.tsx
+++ src/app/AussieLife/page.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "@/components/Footer/page";
 import { Header } from "@/components/Header/page";
+import { Headline } from "@/components/Headline/page";
 import { getBlogsByCategory } from "@/utils/getAllBlogs";
 import Link from "next/link";
 
@@ -10,16 +11,18 @@ export default async function AussieLife() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex-grow">
-        <h1 className="text-2xl">Aussie Life</h1>
-        {posts.map((post) => (
-          <div key={post.slug}>
-            <h2 className="text-xl">{post.frontmatter.title}</h2>
-            <p>{post.frontmatter.date}</p>
-            <Link href={`/AussieLife/${post.slug}`} className="text-blue-500">
-              続きを読む
-            </Link>
-          </div>
-        ))}
+        <Headline category="AussieLife" comment="AussieLife Headline" />
+        <div className="mx-10 flex justify-center flex-wrap gap-6">
+          {posts.map((post) => (
+            <div key={post.slug} className="border p-4 w-[30%]">
+              <h2 className="text-xl">{post.frontmatter.title}</h2>
+              <p>{post.frontmatter.date}</p>
+              <Link href={`/AussieLife/${post.slug}`} className="text-blue-500">
+                続きを読む
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
